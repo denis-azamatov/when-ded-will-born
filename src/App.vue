3f307f69738aa832fb5a0f6b97c54f69c91f5b95
Denis Azamatov <bejsekeevdenis@gmail.com>
2023-11-22 16:28:10 +0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,130 @@
+<template>
+  <div class="timer">
+    <Countdown v-if="date" :date="date"></Countdown>
+    <div v-else>
+      <div>
+        <img loading="lazy" class="gif1" src="./assets/gifs/gif1.gif" alt="">
+        <img loading="lazy" class="gif2" src="./assets/gifs/gif2.gif" alt="">
+        <img loading="lazy" class="gif3" src="./assets/gifs/gif3.gif" alt="">
+        <img loading="lazy" class="gif4" src="./assets/gifs/gif4.gif" alt="">
+        <img loading="lazy" class="gif5" src="./assets/gifs/gif5.gif" alt="">
+        <img loading="lazy" class="gif7" src="./assets/gifs/gif7.webp" alt="">
+        <img loading="lazy" class="gif6" src="./assets/gifs/gif6.gif" alt="">
+      </div>
+      <div class="title">
+        <p>
+          Happy birthday DED!
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import Countdown from './components/Countdown.vue';
+
+function compareDate(first: Date, secondDate: Date): boolean {
+  return first.getFullYear() == secondDate.getFullYear()
+    && first.getDate() == secondDate.getDate()
+    && first.getMonth() == secondDate.getMonth();
+}
+
+/**
+ * Returns next bday or null if it is today
+ */
+function getNext() {
+  const now = new Date();
+  const currentYear = now.getFullYear();
+
+  let target = new Date(currentYear, 10, 26);
+
+  if (target > now) {
+    return target;
+  }
+
+  if (compareDate(now, target)) {
+    return null;
+  }
+
+  return new Date(currentYear + 1, 10, 26);
+}
+
+const date = getNext();
+</script>
+
+
+<style scoped>
+.title {
+  z-index: 2;
+  position: relative;
+}
+
+.title p {
+  font-size: 6rem;
+  animation: blink 1s steps(2, start) infinite;
+}
+
+@keyframes blink {
+  0% {
+    opacity: 0;
+  }
+
+  100% {
+    opacity: 1;
+  }
+}
+
+img {
+  position: absolute;
+  z-index: 1;
+}
+
+.gif1 {
+  top: 5rem;
+}
+
+.gif2 {
+  top: 15rem;
+  left: 5rem
+}
+
+.gif3 {
+  bottom: 3rem;
+  right: 5rem
+}
+
+.gif4 {
+  bottom: 2rem;
+  right: 50rem;
+}
+
+.gif5 {
+  top: 2rem;
+  right: 2rem;
+}
+
+.gif6 {
+  bottom: 2rem;
+  right: 35rem;
+}
+
+.gif7 {
+  top: 2rem;
+  left: 10rem;
+}
+
+@media screen and (max-width: 768px) {
+
+
+  .gif1 {
+    z-index: 10;
+  }
+
+  .gif7 {
+    top: unset;
+    bottom: 2rem;
+    left: 0rem;
+    width: 100%;
+  }
+}
+</style>
---
+++ src/components/Countdown.vue
@@ -0,0 +1,101 @@
+<template>
+    <div class="digit">
+        BIRTHDAY
+    </div>
+    <div class="digit">
+        SCHEDULED
+    </div>
+    <div class="block">
+        <div>
+            <p class="digit">{{ format(days) }}</p>
+            <p class="label">Days</p>
+        </div>
+        <div>
+            <p class="digit">{{ format(hours) }}</p>
+            <p class="label">Hours</p>
+        </div>
+        <div>
+            <p class="digit">{{ format(minutes) }}</p>
+            <p class="label">Minutes</p>
+        </div>
+        <div>
+            <p class="digit">{{ format(seconds) }}</p>
+            <p class="label">Seconds</p>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { computed, defineProps, ref } from 'vue';
+
+const props = defineProps({
+    date: {
+        type: Date,
+        required: true
+    }
+});
+
+const now = ref(Math.trunc((new Date()).getTime() / 1000));
+const target = Math.trunc(props.date.getTime() / 1000);
+
+const interval = window.setInterval(() => {
+    now.value = Math.trunc((new Date()).getTime() / 1000);
+}, 1000);
+
+
+const seconds = computed(() => (target - now.value) % 60);
+const minutes = computed(() => Math.trunc(((target - now.value) / 60) % 60));
+const hours = computed(() => Math.trunc(((target - now.value) / 60 / 60) % 24));
+const days = computed(() => Math.trunc((target - now.value) / 60 / 60 / 24));
+
+function format(value: number) {
+    if (value.toString().length <= 1) {
+        return "0" + value.toString();
+    }
+    return value.toString();
+}
+</script>
+
+<style scoped>
+
+
+.block {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+    gap: 1rem;
+}
+
+.digit{
+    font-size: 6rem;
+    white-space: pre;
+    margin: 0;
+}
+
+.label{
+    margin: 0;
+    font-family: Verdana, Geneva, Tahoma, sans-serif;
+    font-size: 2rem;
+    color: white;
+    text-transform: uppercase;
+}
+
+@media screen and (max-width: 768px) {
+  .digit {
+    font-size: 4rem;
+  }
+
+  .label{
+    font-size: 1.5rem;
+  }
+}
+
+@media screen and (max-width: 520px) {
+  .digit {
+    font-size: 3rem;
+  }
+
+  .label{
+    font-size: 1rem;
+  }
+}
+</style>
